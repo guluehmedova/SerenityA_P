d4ef9be74d78f06a562af814f3cfa07a792f53a8
guluehmedova <guliehmedova19@gmail.com>
2022-08-16 14:22:36 +0400

    repo/services started

---
+++ Smt/Business/Service/DisplayModel.cs
@@ -0,0 +1,19 @@
+﻿using Business.IService;
+using Microsoft.AspNetCore.Mvc;
+using Smt.Default;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Service
+{
+    public class DisplayModel : IDisplayModel
+    {
+        public Task<ActionResult> GetConnect(ModelRow modelrow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Smt/Smt/Smt.Web/Modules/Default/DisplayModel/DisplayModelPage.cs
+++ Smt/Smt/Smt.Web/Modules/Default/DisplayModel/DisplayModelPage.cs
@@ -8,8 +8,6 @@ using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Data.SqlClient;
 using System.Linq;
-using Dapper;
-using System.Data.SqlClient;
 
 namespace Smt.Default.Pages
 {
@@ -17,39 +15,10 @@ namespace Smt.Default.Pages
     [PageAuthorize(typeof(DisplayModelRow))]
     public class DisplayModelController : Controller
     {
-        private readonly IConfiguration _configuration;
-        public DisplayModelController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
         [Route("Default/DisplayModel")]
         public ActionResult Index()
         {
-            using var connection = new SqlConnection(_configuration.GetConnectionString("SmtDefault"));
-            var vendors = connection.Query<VendorRow>("SELECT * FROM vendor.Vendor");
-            var models = connection.Query<ModelRow>("SELECT * FROM model.Model");
-
-            DisplayModelsViewModel displayModelsViewModel = new DisplayModelsViewModel
-            {
-                Vendors = vendors.ToList(),
-                Models = models.ToList()
-            };
-
-            return View("~/Modules/Default/DisplayModel/DisplayModelIndex.cshtml", displayModelsViewModel);
-        }
-        [HttpPut]
-        public IActionResult Check(ModelRow modelrow)
-        {
-            using var connection = new SqlConnection(_configuration.GetConnectionString("SmtDefault"));
-            var existmodels = connection.Query<ModelRow>("SELECT * FROM model.Model");
-
-            var findmodel = existmodels.FirstOrDefault(x => x.ModelId == modelrow.ModelId);
-            if (findmodel == null) { return NotFound(); }
-
-            findmodel.Check = modelrow.Check;
-
-            return View("index", findmodel);
+            return View("~/Modules/Default/DisplayModel/DisplayModelIndex.cshtml");
         }
     }
 }
